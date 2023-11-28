dc51c3800f9a65e20d621d1211c4683232c317a8
YuChen5470 <cyu5470@gmail.com>
2023-11-28 00:31:53 +0000

    upgrades

--- REAL_NEA2023/Assets/DamageFireRate.cs
+++ REAL_NEA2023/Assets/DamageFireRate.cs
@@ -7,14 +7,39 @@ public class DamageFireRate : MonoBehaviour
     // take the current weapon inhand
     // check if currency is ok
     // upgrade a variable 
+    [Header("referencing currentWeapon")]
+    public GunSwitching gunSwitching;
+    public GameObject weaponToUpgrade;
+
+    [Header("Both Weapons")]
+    public GameObject SlingShotOne;
+    public GameObject SlingShotTwo;
+
+    [Header("Getting Script from each object")]
+    public GunScript changeHolderScriptOne;
+    public GunScript changeHolderScriptTwo;
+
     void Start()
     {
-        
+        weaponToUpgrade = gunSwitching.currentWeapon; //current weapon inhand
+        GunScript changeHolderScriptOne = weaponToUpgrade.GetComponent<GunScript>(); //references the gun script inside the gameobject for slingshot 1
+        GunScript changeHolderScriptTwo = weaponToUpgrade.GetComponent<GunScript>(); //references the gun script inside the gameobject for slingshot 2
     }
 
     // Update is called once per frame
-    void Update()
+    public void UpgradeWeaponDelay()
     {
+        //check currency
+
+
+        //upgrades the inhand weapon
+        Debug.Log(weaponToUpgrade.name);
+        if (weaponToUpgrade.name == SlingShotOne.name)
+        {
+           changeHolderScriptOne.timeBetweenShots -= 0.05f; 
+        }else{
+            changeHolderScriptTwo.timeBetweenShots -= 0.05f;
+        }
         
     }
 }
